95c8d15a2ceba9d0d8d9472247695091e2c5f022
Serhii Filatov <filser.spirit@gmail.com>
2023-10-14 18:43:40 +0300

    adds react component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
+import { Card } from './Card';
+
+const imageUrl =
+  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const productsData = [
+  {
+    id: '1',
+    title: 'Tacos With Lime XXL',
+    price: '10.99',
+    discount: 30,
+    imageUrl
+  },
+  {
+    id: '2',
+    title: 'Tacos With Lime XL',
+    price: '6.99',
+    discount: null,
+    imageUrl
+  },
+  {
+    id: '3',
+    title: 'Tacos With Lime M',
+    price: '5.85',
+    discount: 15,
+    imageUrl
+  },
+];
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <section>
+      <h1>Hello FSON89</h1>
+                        {/* тут диструктуризацию */}
+      {productsData.map(product => {
+        return <Product key={product.id} title={product.title} discount={product.discount} price={product.price}/>
+      })}
+      <Card />
+
+    </section>
+  );
+};
+
+
+
+const Product = ({title, price, discount}) => {
+  return (
+    <div>
+      <img src={imageUrl} alt="Tacos With Lime" width="640" />
+      <h2>{title}</h2>
+      {/* {discount && <h3>Discount: {discount}$</h3>} */}
+      {discount ? <h3>Discount: {discount}$</h3> : <p>Sorry, not discount</p>}
+      <p>Price: {price}$</p>
+      <button type="button">Add to cart</button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { App } from 'components/App';
+
+// vanila JS
+// const div = document.createElement('div');
+// const root = document.getElementById('root');
+// root.append(div);
+// div.id = 'some-ID';
+// div.classList = 'test-class';
+
+// const p = document.createElement('p');
+// p.textContent = 'I am P';
+// div.append(p);
+
+// React
+//const paragraph = React.createElement('p', {id: 'test-P-id', children: ['I am P']});
+
+// const p = <p id="test-P-id">I am P</p>;
+
+// const div = React.createElement(
+//   'div',
+//   // props
+//   {
+//     name: 'main',
+//     id: 'test-id',
+//     className: 'test-class',
+//     children: p,
+//   },
+//   // [children]
+// );
+
+// const div = (
+//   <div name="main" id="test-id" class="test-class">
+//     {p}
+//   </div>
+// );
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//   </React.StrictMode>
+// );
